86d4187349a65dae6c7377a128232e01ae360a9f
黄强 <qhuang@iwstech.com>
2023-08-24 09:56:00 +0800

    尝试修复索引器界面统计信息报错的问题

--- app/indexer/indexer.py
+++ app/indexer/indexer.py
@@ -183,4 +183,4 @@ class Indexer(object):
         """
         获取索引器统计信息
         """
-        return self.dbhelper.get_indexer_statistics()
+        return self.dbhelper.get_indexer_statistics(self._client.get_client_id())
