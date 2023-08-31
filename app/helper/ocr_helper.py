d7dfae475cf363d19e5a158385daddfa097b38f9
xuyanling <xuyanling@huami.com>
2023-09-01 01:52:01 +0800

    feat(ocr): 接入百度OCR
    步骤：
    1. 去 https://console.bce.baidu.com/ai/#/ai/ocr/overview/index 申请百度OCR，搜索文字识别
    2. 去 https://console.bce.baidu.com/ai/#/ai/ocr/app/create 创建应用，全选文字识别功能
    3. 去 https://console.bce.baidu.com/ai/#/ai/ocr/overview/resource/getFree 领取免费额度
    4. 去 https://console.bce.baidu.com/ai/#/ai/ocr/app/list 点击创建的应用查看，api_key, secrect_key

--- app/helper/ocr_helper.py
+++ app/helper/ocr_helper.py
@@ -1,11 +1,23 @@
-import base64
+import requests
+import json
+from base64 import b64encode
+from urllib.parse import urlencode
 
-from app.utils import RequestUtils
-
+import log
+from config import Config
+from app.utils import RequestUtils, StringUtils
 
 class OcrHelper:
 
     _ocr_b64_url = "https://movie-pilot.org/captcha/base64"
+    _baiduocr_api_key = None
+    _baiduocr_secret_key = None
+
+    def __init__(self):
+        baidu_ocr = Config().get_config('baiduocr')
+        if baidu_ocr:
+            self._baiduocr_api_key = baidu_ocr.get('api_key', '') or ''
+            self._baiduocr_secret_key = baidu_ocr.get('secret_key', '') or ''
 
     def get_captcha_text(self, image_url=None, image_b64=None, cookie=None, ua=None):
         """
@@ -22,12 +34,63 @@ class OcrHelper:
                 image_bin = ret.content
                 if not image_bin:
                     return ""
-                image_b64 = base64.b64encode(image_bin).decode()
+                image_b64 = b64encode(image_bin).decode()
         if not image_b64:
             return ""
+
+        if self.baiduocr_avaliable():
+            return self.get_captcha_text_by_baiduocr(image_b64=image_b64)
+
         ret = RequestUtils(content_type="application/json").post_res(
             url=self._ocr_b64_url,
             json={"base64_img": image_b64})
         if ret:
             return ret.json().get("result")
         return ""
+
+    def get_captcha_text_by_baiduocr(self, image_b64=None):
+        if not self.baiduocr_avaliable() or not image_b64:
+            return ""
+
+        url = "https://aip.baidubce.com/rest/2.0/ocr/v1/accurate_basic?access_token=" + self.get_baiduocr_access_token()
+        payload = {'image': f'{image_b64}'}
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Accept': 'application/json'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+        if not response:
+            return ""
+        if response.status_code != 200:
+            return ""
+        try:
+            result = response.json()
+            if 'words_result' in result:
+                words_result = result.get('words_result', []) or []
+                if len(words_result) > 0:
+                    captcha_text_list = [result['words'] for result in words_result]
+                    return " ".join(captcha_text_list)
+                else:
+                    return ""
+            else:
+                log.error(f"【OCR】验证码识别失败, 原始返回: {response}")
+                return ""
+        except Exception as e:
+            log.error(f"【OCR】验证码识别失败: {str(e)}")
+            return ""
+
+    def get_baiduocr_access_token(self):
+        """
+        使用 AK，SK 生成鉴权签名（Access Token）
+        :return: access_token，或是None(如果错误)
+        """
+        url = "https://aip.baidubce.com/oauth/2.0/token"
+        params = {"grant_type": "client_credentials", "client_id": self._baiduocr_api_key, "client_secret": self._baiduocr_secret_key}
+        return str(requests.post(url, params=params).json().get("access_token"))
+
+    def baiduocr_avaliable(self):
+        """
+        判断百度OCR是否可用
+        """
+        return StringUtils.is_string_and_not_empty(self._baiduocr_api_key) and StringUtils.is_string_and_not_empty(self._baiduocr_secret_key)
