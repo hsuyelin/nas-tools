4d0c91f21aa0cc485af8875b6f8be1a3481448db
xuyanling <xuyanling@huami.com>
2023-08-31 18:29:22 +0800

    fix(douban sync): 豆瓣同步插件无法手动停止

--- app/plugins/modules/doubansync.py
+++ app/plugins/modules/doubansync.py
@@ -32,7 +32,7 @@ class DoubanSync(_IPluginModule):
     # 主题色
     module_color = "#05B711"
     # 插件版本
-    module_version = "1.0"
+    module_version = "1.1"
     # 插件作者
     module_author = "jxxghp"
     # 作者主页
@@ -73,7 +73,7 @@ class DoubanSync(_IPluginModule):
             self._enable = config.get("enable")
             self._onlyonce = config.get("onlyonce")
             self._sync_type = config.get("sync_type")
-            if str({self._sync_type}) == '1':
+            if self._sync_type == '1':
                 self._interval = 0
                 rss_interval = config.get("rss_interval")
                 if rss_interval and str(rss_interval).isdigit():
@@ -132,7 +132,7 @@ class DoubanSync(_IPluginModule):
                 self.update_config({
                     "onlyonce": self._onlyonce,
                     "enable": self._enable,
-                    "sync_type": str({self._sync_type}),
+                    "sync_type": self._sync_type,
                     "interval": self._interval,
                     "rss_interval": self._rss_interval,
                     "auto_search": self._auto_search,
@@ -151,8 +151,8 @@ class DoubanSync(_IPluginModule):
         return self._enable \
                and self._users \
                and self._types \
-            and ((str({self._sync_type}) == '1' and self._rss_interval)
-                 or (str({self._sync_type}) != '1' and self._interval))
+            and ((self._sync_type == '1' and self._rss_interval)
+                 or (self._sync_type != '1' and self._interval))
 
     @staticmethod
     def get_fields():
@@ -573,7 +573,7 @@ class DoubanSync(_IPluginModule):
         获取每一个用户的每一个类型的豆瓣标记
         :return: 搜索到的媒体信息列表（不含TMDB信息）
         """
-        self.info(f"同步方式：{'近期动态' if str({self._sync_type}) == '1' else '全量同步'}")
+        self.info(f"同步方式：{'近期动态' if self._sync_type == '1' else '全量同步'}")
 
         # 返回媒体列表
         media_list = []
@@ -589,7 +589,7 @@ class DoubanSync(_IPluginModule):
             if userinfo:
                 user_name = userinfo.get("name")
 
-            if str({self._sync_type}) == '1':
+            if self._sync_type != '1':
                 # 每页条数
                 perpage_number = 15
                 # 所有类型成功数量
@@ -712,3 +712,18 @@ class DoubanSync(_IPluginModule):
             # 随机休眠
             sleep(round(random.uniform(1, 5), 1))
         return media_list
+
+    def stop_service(self):
+        """
+        退出插件
+        """
+        try:
+            if self._scheduler:
+                self._scheduler.remove_all_jobs()
+                if self._scheduler.running:
+                    self._event.set()
+                    self._scheduler.shutdown()
+                    self._event.clear()
+                self._scheduler = None
+        except Exception as e:
+            print(str(e))
