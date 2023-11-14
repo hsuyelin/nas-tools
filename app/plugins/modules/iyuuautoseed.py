d278d9e490614653db8c2a03de3f93ffc03614b7
Teddy_Zhu <teddyzhu15@gmail.com>
2023-11-14 09:27:13 +0800

    fix(indexer): 高清城市使用下载直链辅种 (#436)

--- app/plugins/modules/iyuuautoseed.py
+++ app/plugins/modules/iyuuautoseed.py
@@ -64,7 +64,7 @@ class IYUUAutoSeed(_IPluginModule):
         "//a[contains(@href, 'download.php?hash=')]/@href",
         "//a[contains(@href, 'download.php?id=')]/@href",
         "//a[@class='index'][contains(@href, '/dl/')]/@href",
-        "//a[contains(@href, 'download?id=')]/@href",
+        "//input[@title='DirectLink']/@value",
     ]
     _torrent_tags = ["已整理", "辅种"]
     # 待校全种子hash清单
