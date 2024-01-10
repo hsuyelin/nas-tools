b6e3a43cd28018d22be01051b5bbafcac46fa835
jackloves111 <89971817+jackloves111@users.noreply.github.com>
2024-01-11 00:28:35 +0800

    恢复修改

--- app/media/scraper.py
+++ app/media/scraper.py
@@ -655,7 +655,7 @@ class Scraper:
                 # episode thumb
                 if scraper_tv_pic.get("episode_thumb"):
                     episode_thumb = os.path.join(dir_path, file_name + "-thumb.jpg")
-                     if not force_pic \
+                    if not force_pic \
                             and not os.path.exists(episode_thumb):
                         # 优先从TMDB查询
                         episode_image = self.media.get_episode_images(tv_id=media.tmdb_id,
